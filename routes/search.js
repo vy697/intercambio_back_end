590469afe759bbfd4ed3f380027d698cd9035142
Vy Nguyen <vy.p.ngn@gmail.com>
2016-09-14 09:12:49 -0600

    migrations finished

--- routes/search.js
+++ routes/search.js
@@ -4,22 +4,17 @@ var express = require('express'),
     router = express.Router(),
     knex = require('../db/knex.js');
 
-//     function getBooks() {
-//     return knex('book_author')
-//         .join('authors', 'authors.id', 'book_author.author_id')
-//         .join('books', 'books.id', 'book_author.book_id')
-//         .select('books.id', 'books.title', 'books.cover_url', 'books.description', 'books.genre', 'authors.f_name', 'authors.l_name', 'book_author.author_id', 'authors.bio', 'authors.portrait_url');
-// }
-
 function getExchanges() {
   return knex('user_speaks_language')
     .join('languages', 'languages.id', 'user_speaks_language.language_id')
     .join('users', 'users.id', 'user_speaks_language.user_id')
     .join('user_learns_language', 'user_learns_language.user_id', 'user_speaks_language.user_id')
-    .select('users.id as user_id', 'users.email', 'users.pw', 'users.city', 'users.description', 'users.age', 'users.photo_url', 'users.pair', 'users.group', 'users.online', 'users.lang_preference', 'languages.name as speaks_language', 'languages.level as speaks_language_level', 'user_learns_language.language_id as user_learns_language_id');
+    //alias languages table to grab values for user_learns_language name and level
+    .join('languages as l', 'l.id', 'user_learns_language.language_id')
+    .select('users.id as user_id', 'users.name as user_name', 'users.email', 'users.pw', 'users.city', 'users.description', 'users.age', 'users.photo_url', 'users.pair', 'users.group', 'users.online', 'users.lang_preference', 'languages.name as speaks_language', 'languages.level as speaks_language_level', 'user_learns_language.language_id as user_learns_language_id', 'l.name as learns_language', 'l.level as learns_language_level');
 }
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   getExchanges()
   .then(function(data) {
     console.log(data);
--- seeds/languages.js
+++ seeds/languages.js
@@ -5,24 +5,24 @@ exports.seed = function(knex, Promise) {
   return knex('languages').del()
   .then(function() {
     return Promise.all([
-    knex('languages').insert({"id":1,"name":"French","level":"Intermediate"}),
-    knex('languages').insert({"id":2,"name":"Portuguese","level":"Advanced"}),
-    knex('languages').insert({"id":3,"name":"Vietnamese","level":"Advanced"}),
-    knex('languages').insert({"id":4,"name":"Portuguese","level":"Beginner"}),
-    knex('languages').insert({"id":5,"name":"Vietnamese","level":"Native"}),
-    knex('languages').insert({"id":6,"name":"English","level":"Native"}),
-    knex('languages').insert({"id":7,"name":"Portuguese","level":"Advanced"}),
-    knex('languages').insert({"id":8,"name":"Spanish","level":"Intermediate"}),
-    knex('languages').insert({"id":9,"name":"English","level":"Intermediate"}),
-    knex('languages').insert({"id":10,"name":"Spanish","level":"Advanced"}),
-    knex('languages').insert({"id":11,"name":"English","level":"Intermediate"}),
-    knex('languages').insert({"id":12,"name":"Vietnamese","level":"Beginner"}),
-    knex('languages').insert({"id":13,"name":"Portuguese","level":"Native"}),
-    knex('languages').insert({"id":14,"name":"English","level":"Advanced"}),
-    knex('languages').insert({"id":15,"name":"English","level":"Beginner"}),
-    knex('languages').insert({"id":16,"name":"Portuguese","level":"Intermediate"}),
-    knex('languages').insert({"id":17,"name":"French","level":"Advanced"}),
-    knex('languages').insert({"id":18,"name":"French","level":"Native"})
+    knex('languages').insert({"id":1,"name":"Spanish","level":"Beginner"}),
+    knex('languages').insert({"id":2,"name":"Spanish","level":"Intermediate"}),
+    knex('languages').insert({"id":3,"name":"Spanish","level":"Advanced"}),
+    knex('languages').insert({"id":4,"name":"Spanish","level":"Native"}),
+    knex('languages').insert({"id":5,"name":"English","level":"Beginner"}),
+    knex('languages').insert({"id":6,"name":"English","level":"Intermediate"}),
+    knex('languages').insert({"id":7,"name":"English","level":"Advanced"}),
+    knex('languages').insert({"id":8,"name":"English","level":"Native"})
+    // knex('languages').insert({"id":9,"name":"English","level":"Intermediate"}),
+    // knex('languages').insert({"id":10,"name":"Spanish","level":"Advanced"}),
+    // knex('languages').insert({"id":11,"name":"English","level":"Intermediate"}),
+    // knex('languages').insert({"id":12,"name":"Vietnamese","level":"Beginner"}),
+    // knex('languages').insert({"id":13,"name":"Portuguese","level":"Native"}),
+    // knex('languages').insert({"id":14,"name":"English","level":"Advanced"}),
+    // knex('languages').insert({"id":15,"name":"English","level":"Beginner"}),
+    // knex('languages').insert({"id":16,"name":"Portuguese","level":"Intermediate"}),
+    // knex('languages').insert({"id":17,"name":"French","level":"Advanced"}),
+    // knex('languages').insert({"id":18,"name":"French","level":"Native"})
     ]);
   });
 };
